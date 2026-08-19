2b62b988bcaa725ec49d4c85162edc600cff7d51
kroliklove <vikfisfisfis@gmail.com>
2026-08-19 13:32:11 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # config.py
+import os
 
-# Токен бота от BotFather
-BOT_TOKEN = "8794138072:AAG3vyuOg8Cxhce7OkQLEWOmsb9CQCRrJ6s"  # вставь токен
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # берём из переменных Railway
 
-# Ссылки на документы
 OFFER_URL = "https://telegra.ph/PUBLICHNAYA-OFERTA-08-18-27"
 POLICY_URL = "https://telegra.ph/Politika-v-otnoshenii-obrabotki-personalnyh-dannyh-08-18-11"
 
-# Курсы (название, цена, описание)
 COURSES = {
     "buy1": {"name": "Курс 1", "duration": "14 дней", "price": 1500, "description": "Йога для начинающих"},
     "buy2": {"name": "Курс 2", "duration": "21 день", "price": 2000, "description": "Йога для гибкости"},
@@ -16,16 +14,3 @@ COURSES = {
     "buy5": {"name": "Курс 5", "duration": "14 дней", "price": 1800, "description": "Йога для спины"},
     "buy6": {"name": "Курс 6", "duration": "21 день", "price": 2500, "description": "Продвинутая йога"},
 }
-
-# Ссылки на закрытые каналы (после создания)
-CHANNEL_LINKS = {
-    "buy1": "https://t.me/+XXXXXXXXXXXXX",  # пригласительная ссылка
-    "buy2": "https://t.me/+XXXXXXXXXXXXX",
-    "buy3": "https://t.me/+XXXXXXXXXXXXX",
-    "buy4": "https://t.me/+XXXXXXXXXXXXX",
-    "buy5": "https://t.me/+XXXXXXXXXXXXX",
-    "buy6": "https://t.me/+XXXXXXXXXXXXX",
-}
-
-# Токен ЮKassa (позже, когда подключишь)
-YOOKASSA_TOKEN = ""  # пока пусто
